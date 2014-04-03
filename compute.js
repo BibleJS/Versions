57f524b6704042f30dc1e84316df22d99f178b9a
Ionică Bizău <bizauionica@yahoo.com>
2014-04-03 11:38:04 +0300

    Fixes after converting to JSON

---
+++ compute.js
@@ -0,0 +1,15 @@
+// require the file
+var VDDC = require ("./RO/json/verses");
+
+// each object
+for (var i = 0; i < VDDC.length; ++i) {
+
+    // get the current verse
+    var cVDDC = VDDC[i];
+
+    // remove HTML tags
+    cVDDC.text = cVDDC.text.replace(/<\/?[^>]+(>|$)/g, "").replace(/\*/g, "");
+}
+
+// write the fixed json
+require("fs").writeFileSync("./RO/json/verses.json", JSON.stringify(VDDC, null, 4));
